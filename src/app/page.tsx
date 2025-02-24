fb032e9f6d7596c115a0d18bf0c6fce3d54e969e
Антон Задорожний <zadoroshniy_anton_2003@mail.ru>
2025-02-24 18:22:53 +0300

    get users & posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
+
+  //signup
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -29,33 +31,91 @@ export default function Home() {
     console.log(data);
   }
 
+  //users
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch("http://localhost:3001/users");
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  //posts
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:3001/posts");
+    const data = await response.json();
+    console.log(data);
+    setPosts(data);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
 
   return (
     <div className={styles.page}>
-      
-      <h1>Sign up</h1>
+      <div>
+        <h1>Sign up</h1>
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <label>First Name</label>
+          <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
 
-      <div style={{display: "flex", flexDirection: "column"}}>
-        <label>First Name</label>
-        <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+          <label>Last Name</label>
+          <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
 
-        <label>Last Name</label>
-        <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
+          <label>Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
 
-        <label>Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label>Username</label>
+          <input value={username} onChange={(e) => setUsername(e.target.value)} />
 
-        <label>Username</label>
-        <input value={username} onChange={(e) => setUsername(e.target.value)} />
-
-        <label>Password</label>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+          <label>Password</label>
+          <input value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button onClick={signUp}>Sign up</button>
+        </div>
       </div>
 
       <div>
-        <button onClick={signUp}>Sign up</button>
+        <h1>Users</h1>
+        <div className={styles.users_table}>
+          <button onClick={getUsers}>refresh</button>
+          <div className={styles.users_table_row}>
+            <h2>Username</h2>
+            <h2>Email</h2>
+            <h2>First Name</h2>
+            <h2>Last Name</h2>
+          </div>
+
+          {users?.map((user: any) => (
+            <div key={user?.user_id} className={styles.users_table_row}>
+              <p>{user?.username}</p>
+              <p>{user?.email}</p>
+              <p>{user?.first_name}</p>
+              <p>{user?.last_name}</p>
+            </div>
+          ))}
+        </div>
       </div>
 
+      <div>
+        <h1>Posts</h1>
+        <div className={styles.posts_table}>
+          {posts?.map((post: any) => (
+            <div key={post?.post_id} className={styles.posts_table_row}>
+              <h2>{post?.title}</h2>
+              <p>{post?.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
 
     </div>
   );
