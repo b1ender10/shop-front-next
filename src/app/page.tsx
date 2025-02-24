7bfba8286f39143ea011214e28a2d95757f70493
Антон Задорожний <zadoroshniy_anton_2003@mail.ru>
2025-02-24 13:18:55 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,62 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const signUp = async () => {
+    const response = await fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password_hash: password,
+        firstName,
+        lastName
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      
+      <h1>Sign up</h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div style={{display: "flex", flexDirection: "column"}}>
+        <label>First Name</label>
+        <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+
+        <label>Last Name</label>
+        <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
+
+        <label>Email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <label>Username</label>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+
+        <label>Password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+
+      <div>
+        <button onClick={signUp}>Sign up</button>
+      </div>
+
+
     </div>
   );
 }
