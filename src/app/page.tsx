5c423c7da9f426ea76116912c8e381092b0a3364
Антон Задорожний <zadoroshniy_anton_2003@mail.ru>
2025-02-25 17:28:18 +0300

    display all tables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,6 +60,48 @@ export default function Home() {
     getPosts();
   }, []);
 
+  //products
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await fetch("http://localhost:3001/products");
+    const data = await response.json();
+    console.log(data);
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  //orders
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () => {
+    const response = await fetch("http://localhost:3001/orders");
+    const data = await response.json();
+    console.log(data);
+    setOrders(data);
+  }
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  //order items
+  const [orderItems, setOrderItems] = useState([]);
+
+  const getOrderItems = async () => {
+    const response = await fetch("http://localhost:3001/orders-items");
+    const data = await response.json();
+    console.log(data);
+    setOrderItems(data);
+  }
+
+  useEffect(() => {
+    getOrderItems();
+  }, []);
+
 
   return (
     <div className={styles.page}>
@@ -113,6 +155,39 @@ export default function Home() {
             <Post key={post?.post_id} post={post} />
           ))}
         </div>
+      </div>
+
+      <div>
+        <h1>Products</h1>
+        {products?.map((product: any) => (
+          <div key={product?.product_id} style={{display: "flex", gap: "10px"}}>
+            <p>{product?.name}</p>
+            <p>{product?.description}</p>
+            <p>{product?.price}</p>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <h1>Orders</h1>
+        {orders?.map((order: any) => (
+          <div key={order?.order_id} style={{display: "flex", gap: "10px"}}>
+            <p>{order?.user_id}</p>
+            <p>{order?.total_amount}</p>
+            <p>{order?.status}</p>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <h1>Orders Items</h1>
+        {orderItems?.map((orderItem: any) => (
+          <div key={orderItem?.order_item_id} style={{display: "flex", gap: "10px"}}>
+            <p>{orderItem?.order_id}</p>
+            <p>{orderItem?.product_id}</p>
+            <p>{orderItem?.quantity}</p>
+          </div>
+        ))}
       </div>
 
     </div>
